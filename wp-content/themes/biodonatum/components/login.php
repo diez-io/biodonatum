dea40e90aeb49e7de8f9492c0ac7819b42b8497d
Alexander Kosarev <alexcdt0494@gmail.com>
2024-12-27 14:02:48 +0200

    new client form handler

--- wp-content/themes/biodonatum/components/login.php
+++ wp-content/themes/biodonatum/components/login.php
@@ -7,20 +7,26 @@
                 Создать учетную запись
             </span>
         </div>
-        <form class="form" data-form>
+        <form class="form custom-woocommerce-form" action="<?= esc_url(admin_url('admin-ajax.php')) ?>">
+            <input type="hidden" name="rememberme" value="forever">
+            <?php wp_nonce_field( 'woocommerce-login', 'woocommerce-login-nonce' ); ?>
+            <input type="hidden" name="action" value="custom_login">
+
             <div class="registration__input">
                 <div class="registration__input--lable">Адрес электронной почты:</div>
-                <input class="input" type="text" name="email" placeholder="Введите E-mail">
+                <input class="input input--required" type="text" name="username" placeholder="Введите E-mail">
+                <div class="wpcf7-not-valid-tip error_invalid_username" style="display:none;"></div>
             </div>
             <div class="registration__input">
                 <div class="registration__input--lable">Пароль:</div>
-                <input class="input" type="password" name="password" placeholder="Введите пароль">
+                <input class="input input--required" type="password" name="password" placeholder="Введите пароль">
+                <div class="wpcf7-not-valid-tip" style="display:none;"></div>
             </div>
             <div class="registration__btns">
                 <button type="submit" class="button">Войти</button>
                 <div class="registration__btns--forgot">Забыли пароль?</div>
             </div>
-            <input type="checkbox" style="display: none;"  data-agree required>
+            <input type="checkbox" style="display: none;"  data-agree>
             <div data-agree-custom class="noselect">
                 <svg>
                     <use xlink:href="<?= get_template_directory_uri(); ?>/assets/sprite.svg#icon-checkbox"></use>
@@ -32,6 +38,8 @@
                     By clicking the button you agree to the terms of the <span>Privacy Policy</span>
                 </div>
             </div>
+            <span class="wpcf7-spinner"></span>
+            <div class="wpcf7-response-output" aria-hidden="true" style="display:none;"></div>
         </form>
     </div>
     <div class="registration--reset_password" style="display:none;">
@@ -42,7 +50,7 @@
                 <input class="input" type="text" name="email" placeholder="Введите E-mail">
             </div>
             <button type="submit" class="button button--wide">Отправить ссылку для восстановления пароля</button>
-            <input type="checkbox" style="display: none;"  data-agree required>
+            <input type="checkbox" style="display: none;"  data-agree>
             <div data-agree-custom class="noselect">
                 <svg>
                     <use xlink:href="<?= get_template_directory_uri(); ?>/assets/sprite.svg#icon-checkbox"></use>
@@ -58,15 +66,16 @@
     </div>
     <div class="registration--registration" style="display:none;">
         <div class="registration__title">Регистрация</div>
-        <form class="form" data-form action="<?= esc_url(admin_url('admin-ajax.php')) ?>">
+        <form class="form custom-woocommerce-form" action="<?= esc_url(admin_url('admin-ajax.php')) ?>">
             <? wp_nonce_field('woocommerce-register', 'woocommerce-register-nonce'); ?>
             <input type="hidden" name="action" value="custom_register">
             <div class="registration__input">
                 <div class="registration__input--lable">Адрес электронной почты:</div>
                 <input class="input" type="text" name="email" placeholder="Введите E-mail">
+                <div class="wpcf7-not-valid-tip error_invalid_username" style="display:none;"></div>
             </div>
             <button type="submit" class="button button--wide">Зарегистрироваться</button>
-            <input type="checkbox" style="display: none;"  data-agree required>
+            <input type="checkbox" style="display: none;"  data-agree>
             <div data-agree-custom class="noselect">
                 <svg>
                     <use xlink:href="<?= get_template_directory_uri(); ?>/assets/sprite.svg#icon-checkbox"></use>
@@ -78,6 +87,8 @@
                     By clicking the button you agree to the terms of the <span>Privacy Policy</span>
                 </div>
             </div>
+            <span class="wpcf7-spinner"></span>
+            <div class="wpcf7-response-output" aria-hidden="true" style="display:none;"></div>
         </form>
     </div>
 </div>
---
+++ wp-content/themes/biodonatum/js/common.js
@@ -0,0 +1,195 @@
+jQuery(function ($) {
+    class Form {
+        $form;
+        $submit;
+        $agreeCheckbox;
+        $customCheckbox;
+        errors;
+        url;
+        inputs;
+        requiredInputs;
+        $responseOutput;
+        notValidTips;
+
+        constructor($form) {
+            this.$form = $form;
+            this.$submit = this.$form.find('[type="submit"]');
+            this.$agreeCheckbox = this.$form.find('[data-agree]');
+            this.$customCheckbox = this.$form.find('[data-agree-custom]');
+            this.url = this.$form.attr('action');
+            this.inputs = this.$form.find('input, textarea');
+            this.requiredInputs = this.inputs.filter('.input--required');
+            this.errors = [];
+            this.$responseOutput = this.$form.find('.wpcf7-response-output');
+            this.notValidTips = this.$form.find('.wpcf7-not-valid-tip');
+            this.init();
+        }
+
+        init() {
+            this.changeBtnStatus();
+            this.toggleAgree();
+
+            this.$submit.on('click', (e) => {
+                e.preventDefault();
+                this.$responseOutput.hide();
+                this.inputs.removeClass('wpcf7-not-valid');
+                this.notValidTips.hide();
+                this.validate();
+            });
+
+            this.$customCheckbox && this.$customCheckbox.on('click', e => {
+                if (e.target.tagName === 'SPAN') {
+                    // open privacy policy
+                }
+                else {
+                    this.toggleAgreeCustom();
+                }
+            });
+
+            this.inputs.on('input', () => this.changeBtnStatus());
+        }
+
+        validate() {
+            let error = false; // Начальное значение false
+
+            this.inputs.each((i, input) => {
+                console.log(common.staticContent.fill_out_this_field);
+                // Исключаем скрытые поля и те, которые не требуют валидации
+                if (input.type === 'hidden' || input.name === 'AUTH_FORM' || input.name === 'TYPE' || input.name === 'backurl' || input.name === 'remember' || input.type === 'checkbox') {
+                    return; // Пропускаем эти поля
+                }
+                // Проверяем длину значения
+                if (input.value.length < 3) {
+                    $(input).toggleClass('wpcf7-not-valid', true);
+                    const $notValidTip = $(input).siblings('.wpcf7-not-valid-tip');
+                    $notValidTip.text(common.staticContent.fill_out_this_field);
+                    $notValidTip.show();
+                    error = true;
+                }
+            });
+            // Если ошибок нет, отправляем данные
+            if (!error) {
+                this.sendData()
+            }
+        }
+
+        getData() {
+            const formData = new FormData();
+
+            this.inputs.each((i, input) => {
+                if (input.name) {
+                    formData.append(input.name, input.value);
+                }
+            });
+
+            return formData;
+        }
+
+        sendData() {
+            this.$form.toggleClass('submitting');
+
+            $.ajax({
+                url: this.url,
+                method: "POST",
+                data: this.getData(),
+                processData: false,
+                contentType: false,
+                success: (response, status, xhr) => {
+                    const contentType = xhr.getResponseHeader('Content-Type');
+
+                    console.log(response);
+
+                    if (contentType && contentType.includes('text/html')) {
+                        console.warn('Received HTML instead of expected JSON');
+                    }
+                    else if (response.success) {
+                        //this.resetInputs(); // Reset input values
+                    }
+
+                    if (response.redirect) {
+                        location.href = response.redirect;
+                    }
+
+                    this.showResult(response);
+                },
+                error: (xhr, status, error) => {
+                    console.error('Error during submission:', error);
+                },
+                complete: () => {
+                    this.$form.removeClass('submitting');
+                }
+            });
+        }
+
+        showResult(response) {
+            if (response.message) {
+                this.$responseOutput.text(response.message);
+                this.$responseOutput.show();
+
+                if (response.errors) {
+                    for (let [code, error] of Object.entries(response.errors)) {
+                        const $notValidTip = this.notValidTips.filter('.error_' + code);
+                        $notValidTip.siblings('.input').toggleClass('wpcf7-not-valid', true);
+                        $notValidTip.text(error);
+                        $notValidTip.show();
+                    }
+                }
+            }
+        }
+
+        resetInputs() {
+            this.inputs.forEach(input => {
+                if (input.tagName === 'INPUT') {
+                    const inputElement = input; // Явно указываем тип как HTMLInputElement
+                    // Проверяем тип инпута
+                    if (inputElement.type === 'checkbox') {
+                        inputElement.checked = false; // Сбрасываем чекбоксы
+                    } else {
+                        inputElement.value = ''; // Обнуляем значение для текстовых полей
+                    }
+                } else if (input.tagName === 'TEXTAREA') {
+                    const textareaElement = input; // Явно указываем тип как HTMLTextAreaElement
+                    textareaElement.value = ''; // Обнуляем значение для текстовых областей
+                }
+            });
+        }
+
+
+        changeBtnStatus() {
+            let isActive = true;
+
+            if (this.$agreeCheckbox) {
+                isActive = this.$agreeCheckbox.checked;
+            }
+
+            if (isActive) {
+                this.requiredInputs.each(function () {
+                    return (isActive = $(this).val() !== '');
+                });
+            }
+
+            this.$submit[0].toggleAttribute('disabled', !isActive);
+        }
+
+        toggleAgree() {
+            if (this.$agreeCheckbox) {
+                this.$agreeCheckbox.on('change', () => {
+                    this.changeBtnStatus();
+                });
+            }
+        }
+
+        toggleAgreeCustom() {
+            this.$customCheckbox.find('svg').toggle();
+
+            this.$agreeCheckbox.checked = !this.$agreeCheckbox.checked;
+            this.changeBtnStatus();
+        }
+    }
+
+    $('.custom-woocommerce-form').each(function() {
+        console.log('each custom-woocommerce-form');
+        new Form($(this));
+    });
+});
+
