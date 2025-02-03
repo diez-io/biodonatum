1fc2180226755706a594799ff3fbd5cee1c862af
Alexander Kosarev <alexcdt0494@gmail.com>
2025-02-03 13:07:38 +0200

    fix video animation for mobile

--- src/js/app/videoAnimation.ts
+++ src/js/app/videoAnimation.ts
@@ -10,6 +10,8 @@ class VideoAnimation {
     tuneDownwardSpeed: number;
     maxWidth: number;
     startDelta: number;
+    previousTouch: number;
+    supportedPlaybackRange: [number, number];
 
     constructor(el: Element) {
         this.el = el;
@@ -19,11 +21,19 @@ class VideoAnimation {
         this.tuneDownwardSpeed = 1500;
 
         this.maxWidth = 1610;
+        this.supportedPlaybackRange = [0, 0];
+        //this.findSupportedPlaybackRange();
+
+        const isMobile = window.innerWidth < 768;
+        this.video.src = isMobile ? this.video.dataset.videoSrcMob : this.video.dataset.videoSrc;
+        this.video.load();
 
         setTimeout(() => this.init(), 2000);
     }
 
     init() {
+        this.video.play();
+        this.video.pause();
         this.videoDuration = this.video.duration;
         this.startDelta = 2 * window.innerHeight / 3;
 
@@ -43,7 +53,7 @@ class VideoAnimation {
 
             event.preventDefault();
 
-            if (deltaY < 0) {
+            //if (deltaY < 0) {
                 //const scrollDelta = Math.sign(deltaY) * 0.001;
                 const scrollDelta = deltaY / this.tuneDownwardSpeed;
 
@@ -59,80 +69,180 @@ class VideoAnimation {
                 });
 
                 window.addEventListener("wheel", lockScrollAndSyncVideo, { passive: false });
-            }
-            else if (this.video.currentTime !== this.videoDuration) {
-                this.video.play();
-                let playbackRate = Math.abs(deltaY) / this.tuneForwardSpeed;
-                playbackRate = playbackRate > 16 ? 16 : playbackRate;
+            //}
+            //else if (this.video.currentTime !== this.videoDuration) {
+                // this.video.play();
+                // let playbackRate = Math.abs(deltaY) / this.tuneForwardSpeed;
+                // playbackRate = playbackRate < this.supportedPlaybackRange[0] ? this.supportedPlaybackRange[0] : playbackRate;
+                // playbackRate = playbackRate > this.supportedPlaybackRange[1] ? this.supportedPlaybackRange[1] : playbackRate;
 
-                this.video.playbackRate = playbackRate;
+                // console.log('playbackRate', playbackRate);
 
-                window.removeEventListener("wheel", lockScrollAndSyncVideo);
+                // this.video.playbackRate = playbackRate;
 
-                await new Promise(resolve => {
-                    setTimeout(resolve, 50);
-                });
+                // window.removeEventListener("wheel", lockScrollAndSyncVideo);
 
-                window.addEventListener("wheel", lockScrollAndSyncVideo, { passive: false });
+                // await new Promise(resolve => {
+                //     setTimeout(resolve, 300);
+                // });
 
-                this.video.pause();
-            }
+                // window.addEventListener("wheel", lockScrollAndSyncVideo, { passive: false });
+
+                // this.video.pause();
+
+
+
+                // const scrollDelta = deltaY / this.tuneDownwardSpeed;
+
+                // window.removeEventListener("wheel", lockScrollAndSyncVideo);
+
+                // this.video.currentTime = Math.min(
+                //     Math.max(this.video.currentTime + scrollDelta * this.videoDuration, 0),
+                //     this.videoDuration
+                // );
+
+                // await new Promise(resolve => {
+                //     setTimeout(resolve, 50);
+                // });
+
+                // window.addEventListener("wheel", lockScrollAndSyncVideo, { passive: false });
+
+            //}
+
+            console.log(this.videoDuration, this.video.currentTime);
 
             if (this.video.currentTime === 0) {
                 this.position = 'down'
                 unlockScroll();
             }
-            else if (this.video.currentTime === this.videoDuration) {
+            else if (this.videoDuration - this.video.currentTime <= 0.0001) {
                 this.position = 'up';
                 unlockScroll();
             }
         };
 
         const lockScroll = () => {
+            console.log('lockScroll');
             isScrollLocked = true;
-            document.body.style.overflow = "hidden";
+            this.el.scrollIntoView(true);
+            document.body.style.overflow = 'hidden';
+            this.video.classList.add('video-container--fixed');
         };
 
         const unlockScroll = () => {
+            console.log('unlockScroll');
+            this.el.scrollIntoView(true);
             isScrollLocked = false;
-            document.body.style.overflow = "";
+            document.body.style.overflow = '';
+            this.video.classList.remove('video-container--fixed');
         };
 
-        window.addEventListener("scroll", () => {
+        window.addEventListener("scroll", (e) => {
             const rect = this.el.getBoundingClientRect();
+            console.log('scroll', rect.top);
 
             if (!isScrollLocked) {
+                if (this.position === 'down' && rect.top <= this.startDelta && rect.top > 0) {
+                    this.transformContainer(rect.top);
+                }
+
+                if (this.position === 'up' && (window.innerHeight - rect.bottom) <= this.startDelta && (window.innerHeight - rect.bottom) > 0) {
+                    this.transformContainer(window.innerHeight - rect.bottom);
+                }
+
                 if (
                     (this.position === 'up' && window.innerHeight - rect.bottom <= 0) ||
                     (this.position === 'down' && rect.top <= 0)
                 ) {
-                    this.el.scrollIntoView(true);
                     lockScroll();
                 }
             }
-
-            if (this.position === 'down' && rect.top <= this.startDelta) {
-                this.transformContainer(rect.top);
-            }
-
-            if (this.position === 'up' && (window.innerHeight - rect.bottom) <= this.startDelta) {
-                this.transformContainer(window.innerHeight - rect.bottom);
+            else {
+                e.preventDefault();
+                e.stopImmediatePropagation();
+                e.stopPropagation();
             }
         });
 
         window.addEventListener("wheel", lockScrollAndSyncVideo, { passive: false });
+
+        window.addEventListener("touchstart", (event: TouchEvent) => {
+            if (event.touches.length === 1) {
+                this.previousTouch = event.touches[0].clientY;
+            }
+        }, { passive: false });
+
+        window.addEventListener("touchmove", async (event: TouchEvent) => {
+            if (isScrollLocked) {
+                event.preventDefault();
+                event.stopImmediatePropagation();
+                event.stopPropagation();
+            }
+
+            if (event.touches.length === 1) {
+                const deltaTouch = this.previousTouch - event.touches[0].clientY;
+                this.simulateWheelEvent(deltaTouch);
+                this.previousTouch = event.touches[0].clientY;
+            }
+        }, { passive: false });
+
+        // window.addEventListener("touchend", (event: TouchEvent) => {
+        //     console.log('touchend');
+        // });
     }
 
     transformContainer = (delta: number) => {
         const width = this.maxWidth + ((this.startDelta - (delta)) / this.startDelta) * (window.innerWidth - this.maxWidth);
-        const height = 40 + ((this.startDelta - delta) / this.startDelta) * 60;
         const padding = 20 - ((this.startDelta - delta) / this.startDelta) * 20;
 
         (this.el as HTMLElement).style.maxWidth = width + 'px';
-        (this.el as HTMLElement).style.height = height + 'vh';
         (this.el as HTMLElement).style.padding = '0 ' + padding + 'px';
         (this.video as HTMLElement).style.borderRadius = padding + 'px';
+    };
+
+    simulateWheelEvent(deltaY: number) {
+        const simulatedEvent = new WheelEvent("wheel", {
+            deltaY: deltaY,
+            bubbles: true,
+            cancelable: true,
+        });
+
+        window.dispatchEvent(simulatedEvent);
     }
+
+    findSupportedPlaybackRange = () => {
+        let slowestMin = 0.0001;
+        let slowestMax = 1.0;
+
+        while (slowestMax - slowestMin >= 0.0001) {
+            const slowest = (slowestMin + slowestMax) / 2;
+
+            try {
+                this.video.playbackRate = slowest;
+                slowestMax = slowest;
+            } catch (error) {
+                slowestMin = slowest;
+            }
+        }
+
+        this.supportedPlaybackRange[0] = slowestMax;
+
+        let fastestMin = 1.0;
+        let fastestMax = 30.0;
+
+        while (fastestMax - fastestMin >= 0.0001) {
+            const fastest = (fastestMin + fastestMax) / 2;
+
+            try {
+                this.video.playbackRate = fastest;
+                fastestMin = fastest;
+            } catch (error) {
+                fastestMax = fastest;
+            }
+        }
+
+        this.supportedPlaybackRange[1] = fastestMin;
+    };
 }
 
 export default VideoAnimation;
