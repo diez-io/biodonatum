0fc1af061d60d4278ec640c8b09576da6364a29d
Alexander Kosarev <alexcdt0494@gmail.com>
2025-02-04 18:39:01 +0200

    calibrate animation speed

--- wp-content/themes/biodonatum/components/videoAnimation.php
+++ wp-content/themes/biodonatum/components/videoAnimation.php
@@ -1,6 +1,10 @@
 <section class="section">
     <div class="video-container video-animation">
-        <video id="scroll-video" playsinline muted data-video-src="<?= wp_get_attachment_url(get_static_content('video_animation')) ?>" data-video-src-mob="<?= wp_get_attachment_url(get_static_content('video_animation_mob')) ?>">
-        </video>
+        <div class="scroll-video-wrapper">
+            <video class="scroll-video scroll-video__forward" playsinline muted data-video-src="<?= wp_get_attachment_url(get_static_content('video_animation')) ?>" data-video-src-mob="<?= wp_get_attachment_url(get_static_content('video_animation_mob')) ?>">
+            </video>
+            <video class="scroll-video scroll-video__backward" playsinline muted data-video-src="<?= wp_get_attachment_url(get_static_content('video_animation_reverse')) ?>" data-video-src-mob="<?= wp_get_attachment_url(get_static_content('video_animation_mob_reverse')) ?>">
+            </video>
+        </div>
     </div>
 </section>
