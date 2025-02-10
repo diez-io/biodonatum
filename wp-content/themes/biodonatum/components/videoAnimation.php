4952cc20661c1977163231a5651a8e083cf23e8e
Alexander Kosarev <alexcdt0494@gmail.com>
2025-02-10 17:05:06 +0200

    add video loading

--- wp-content/themes/biodonatum/components/videoAnimation.php
+++ wp-content/themes/biodonatum/components/videoAnimation.php
@@ -1,6 +1,15 @@
 <section class="section">
     <div class="video-container video-animation">
         <div class="scroll-video-wrapper">
+            <div class="video-container__loader">
+                <picture>
+                    <img class="video-container__loader__desktop" src="<?= wp_get_attachment_url(get_static_content('video_animation_preview')) ?>" alt="">
+                    <img class="video-container__loader__mob" src="<?= wp_get_attachment_url(get_static_content('video_animation_preview_mob')) ?>" alt="">
+                </picture>
+                <div class="video-container__loader__spinner">
+                    <div class="video-container__loader__spinner__element"></div>
+                </div>
+            </div>
             <video class="scroll-video scroll-video__forward" playsinline muted data-video-src="<?= wp_get_attachment_url(get_static_content('video_animation')) ?>" data-video-src-mob="<?= wp_get_attachment_url(get_static_content('video_animation_mob')) ?>">
             </video>
             <video class="scroll-video scroll-video__backward" playsinline muted data-video-src="<?= wp_get_attachment_url(get_static_content('video_animation_reverse')) ?>" data-video-src-mob="<?= wp_get_attachment_url(get_static_content('video_animation_mob_reverse')) ?>">
