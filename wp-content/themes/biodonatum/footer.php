93e7fda4ad2ef71c9694449cedb11dcfad34727e
Alexander Kosarev <alexcdt0494@gmail.com>
2025-06-26 10:08:08 +0200

    add cookie policy page

--- wp-content/themes/biodonatum/footer.php
+++ wp-content/themes/biodonatum/footer.php
@@ -23,7 +23,8 @@
                     <a href="<?= home_url("/$language_slug" . parse_url(get_permalink(get_page_by_path('loyalty-program')), PHP_URL_PATH)); ?>" class="footer__link"><?= get_static_content('loyalty_program') ?></a>
                     <a href="<?= home_url("/$language_slug" . parse_url(get_permalink(get_page_by_path('privacy-policy')), PHP_URL_PATH)); ?>" class="footer__link"><?= get_static_content('privacy_policy') ?></a>
                     <a href="<?= home_url("/$language_slug" . parse_url(get_permalink(get_page_by_path('vacancy')), PHP_URL_PATH)); ?>" class="footer__link"><?= get_static_content('vacancy') ?></a>
-                    <a href="<?= home_url("/$language_slug" . parse_url(get_permalink(get_page_by_path('partners')), PHP_URL_PATH)); ?>" class="footer__link"><?= get_static_content('partners') ?></a>
+                    <!-- <a href="<?= home_url("/$language_slug" . parse_url(get_permalink(get_page_by_path('partners')), PHP_URL_PATH)); ?>" class="footer__link"><?= get_static_content('partners') ?></a> -->
+                    <a href="<?= home_url("/$language_slug" . parse_url(get_permalink(get_page_by_path('cookie-policy')), PHP_URL_PATH)); ?>" class="footer__link"><?= get_static_content('cookie_policy') ?></a>
                     <a href="<?= home_url("/$language_slug" . parse_url(get_permalink(get_page_by_path('partnership')), PHP_URL_PATH)); ?>" class="footer__link"><?= get_static_content('partnership') ?></a>
                 </nav>
             </div>
---
+++ wp-content/themes/biodonatum/page-cookie_policy.php
@@ -0,0 +1,35 @@
+<?php
+/* Template Name: Cookie Policy Page */
+?>
+<? get_header(); ?>
+
+            <main class="main">
+                <section class="section pt-50">
+                    <div class="container">
+                        <div class="breadcrumbs mb-40">
+                            <a href="<?= home_url(); ?>" class="breadcrumbs__link"><?= get_static_content('home') ?></a>
+                            <span class="breadcrumbs__link"><?= get_static_content('cookie_policy') ?></span>
+                        </div>
+                        <div class="head">
+                            <h2 class="title"><?= get_static_content('cookie_policy') ?></h2>
+                        </div>
+
+                        <? if (get_static_content('cookie_policy_file')) : ?>
+                            <a target="_blank" class="download-terms-link" href="<?= wp_get_attachment_url(get_static_content('cookie_policy_file')) ?>">
+                                <div class="download-terms-link__icon">
+                                    <svg>
+                                        <use xlink:href="<?= get_template_directory_uri(); ?>/assets/sprite.svg#icon-pdf"></use>
+                                    </svg>
+                                </div>
+                                <div class="download-terms-link__text">
+                                    <?= get_static_content('download') . ' ' . get_static_content('cookie_policy') ?>
+                                </div>
+                            </a>
+                        <? endif; ?>
+                    </div>
+                </section>
+
+                <? get_template_part('components/feedback'); ?>
+            </main>
+
+<? get_footer(); ?>
